b0440d1133d3c070b8d22400060d300ff3b989a6
Farhan Arshad <farhan3@users.noreply.github.com>
2018-11-07 10:44:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! modified')
+  response.send('Welcome to Huawei Public Cloud Service Stage Service<br>Application Version: 2')
 })
 
 app.listen(app.get('port'), function() {
